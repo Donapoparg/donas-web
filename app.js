97b6658bb015d6a8190e98a3163a71143699b9a9
Donapoparg <agusparaversifunciona@gmail.com>
2025-12-07 20:58:28 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+// INICIO DEL CÓDIGO A COPIAR
+import React, { useState, useEffect, useRef, memo } from 'https://esm.sh/react@18.2.0';
+import { createRoot } from 'https://esm.sh/react-dom@18.2.0/client';
+// ... todo el código hasta el final ...
+import { jsx as _jsx } from "react/jsx-runtime";
+root.render(/*#__PURE__*/_jsx(App, {}));
+// FIN DEL CÓDIGO A COPIAR
