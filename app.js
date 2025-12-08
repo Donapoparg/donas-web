eeffb9d5b7e834e7c9cf10fc0cef9990ed2568ba
Donapoparg <agusparaversifunciona@gmail.com>
2025-12-07 21:22:54 -0300

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-// INICIO DEL CÓDIGO A COPIAR
-import React, { useState, useEffect, useRef, memo } from 'https://esm.sh/react@18.2.0';
-import { createRoot } from 'https://esm.sh/react-dom@18.2.0/client';
-// ... todo el código hasta el final ...
-root.render(/*#__PURE__*/_jsx(App, {}));
-// FIN DEL CÓDIGO A COPIAR
